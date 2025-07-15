87755b015c38090599e3be88f3f764d8210ccf63
typovrak <typovrak@gmail.com>
2025-07-15 20:40:15 +0200

    split parser.go into multiple files

--- line-type.go
+++ line-type.go
@@ -3,8 +3,20 @@ package typotestcolor
 import (
 	"bytes"
 	"io"
-	"strconv"
 )
+
+type LineType struct {
+	Title   LineTypeTitle
+	Summary LineTypeSummary
+}
+
+type LineTypeTitle struct {
+	Colors      ANSIConfig
+	Prefix      string
+	Suffix      string
+	Hide        bool
+	Aggregation LineTypeTitleAggregation
+}
 
 func AddLineFeedBetweenErrorThrown(opts Opts, w io.Writer, errorBefore *bool, isError bool) {
 	Debug(opts, "AddLineFeedBetweenErrorThrown")
@@ -32,102 +44,6 @@ func HandleLineType(
 	AddLineFeedBetweenErrorThrown(opts, w, errorBefore, isError)
 	*line = bytes.Replace(*line, defaultTitleType, []byte(lineType.Title.Prefix), 1)
 	*line = append(*line, []byte(lineType.Title.Suffix)...)
-}
-
-func PrintAggregation(opts Opts, optsTypeTitleAggregation LineTypeTitleAggregation, aggregationCount *AggregationCount, aggregationLines *[]byte) {
-	if aggregationCount.Value >= optsTypeTitleAggregation.Threshold && len(aggregationCount.Lines) > 2 {
-		// first line
-		*aggregationLines = append(*aggregationLines, aggregationCount.Lines[0]...)
-
-		// setup aggregation color
-		*aggregationLines = append(*aggregationLines, ColorANSI(opts, optsTypeTitleAggregation.Colors)...)
-
-		// aggregation prefix
-		*aggregationLines = append(*aggregationLines, []byte(optsTypeTitleAggregation.Prefix)...)
-
-		// remove first and last line (-2)
-		*aggregationLines = append(*aggregationLines, []byte(strconv.Itoa(aggregationCount.Value-2))...)
-
-		// aggregation suffix
-		*aggregationLines = append(*aggregationLines, []byte(optsTypeTitleAggregation.Suffix)...)
-		*aggregationLines = append(*aggregationLines, []byte("\n")...)
-
-		// reset aggregation color
-		*aggregationLines = append(*aggregationLines, ColorReset...)
-
-		// last line
-		lastLine := aggregationCount.Lines[len(aggregationCount.Lines)-1]
-		*aggregationLines = append(*aggregationLines, bytes.TrimLeft(lastLine, "\n")...)
-		return
-	}
-
-	// no aggregation output
-	for i := 0; i < len(aggregationCount.Lines); i++ {
-		*aggregationLines = append(*aggregationLines, aggregationCount.Lines[i]...)
-	}
-}
-
-func GetOptsTypeTitleAggregationFromAggregationCountType(opts Opts, aggregationCount *AggregationCount) LineTypeTitleAggregation {
-	switch aggregationCount.Type {
-	// run
-	case AggregationTypeRun:
-		return opts.Run.Title.Aggregation
-
-	// fail
-	case AggregationTypeFail:
-		return opts.Fail.Title.Aggregation
-
-	// pass
-	case AggregationTypePass:
-		return opts.Pass.Title.Aggregation
-
-	// skip
-	case AggregationTypeSkip:
-		return opts.Skip.Title.Aggregation
-
-	// failed
-	case AggregationTypeFailed:
-		return opts.Failed.Title.Aggregation
-
-	// ok
-	case AggregationTypeOk:
-		return opts.Ok.Title.Aggregation
-
-	// error thrown
-	case AggregationTypeErrorThrown:
-		return opts.ErrorThrown.Title.Aggregation
-
-	default:
-		return LineTypeTitleAggregation{}
-	}
-}
-
-func HandleAggregation(opts Opts, lineType LineType, aggregationCount *AggregationCount, aggregationType AggregationType, aggregationLines *[]byte, formattedLine *[]byte) {
-	if aggregationCount.Type != aggregationType {
-		optsTypeTitleAggregation := GetOptsTypeTitleAggregationFromAggregationCountType(opts, aggregationCount)
-		PrintAggregation(opts, optsTypeTitleAggregation, aggregationCount, aggregationLines)
-	}
-
-	if lineType.Title.Aggregation.Activate {
-		if aggregationCount.Type != aggregationType {
-			aggregationCount.Type = aggregationType
-			aggregationCount.Value = 0
-			aggregationCount.Lines = [][]byte{*formattedLine}
-		} else {
-			aggregationCount.Lines = append(aggregationCount.Lines, *formattedLine)
-		}
-
-		aggregationCount.Value++
-		*formattedLine = nil
-		return
-	}
-
-	// no aggregation
-	if aggregationCount.Type != AggregationTypeNone {
-		aggregationCount.Type = AggregationTypeNone
-		aggregationCount.Value = 0
-		aggregationCount.Lines = [][]byte{}
-	}
 }
 
 func FormatTestEndLine(line []byte, formattedLine *[]byte, color []byte) {
@@ -280,66 +196,4 @@ func FormatTestLine(
 	}
 
 	return nil, nil
-}
-
-func AddPrintLineSummary(print *[]byte, opts Opts, summary LineTypeSummary, value int) {
-	if len(*print) == 0 {
-		// manage header summary
-		*print = append(*print, ColorANSI(opts, opts.Summary.Header.Colors)...)
-		*print = append(*print, []byte(opts.Summary.Header.Title)...)
-		*print = append(*print, ColorReset...)
-		*print = append(*print, []byte("\n")...)
-	}
-
-	// manage summary content
-	*print = append(*print, ColorANSI(opts, summary.Colors)...)
-	*print = append(*print, []byte(summary.Prefix)...)
-	*print = append(*print, []byte(strconv.Itoa(value))...)
-	*print = append(*print, []byte(summary.Suffix)...)
-
-	// manage end of line
-	*print = append(*print, ColorReset...)
-	*print = append(*print, []byte("\n")...)
-}
-
-func PrintLineSummary(opts Opts, lineSummary LineSummary) []byte {
-	var print []byte
-
-	if !opts.Run.Summary.Hide {
-		AddPrintLineSummary(&print, opts, opts.Run.Summary, lineSummary.Run)
-	}
-
-	if !opts.Fail.Summary.Hide {
-		AddPrintLineSummary(&print, opts, opts.Fail.Summary, lineSummary.Fail)
-	}
-
-	if !opts.Pass.Summary.Hide {
-		AddPrintLineSummary(&print, opts, opts.Pass.Summary, lineSummary.Pass)
-	}
-
-	if !opts.Skip.Summary.Hide {
-		AddPrintLineSummary(&print, opts, opts.Skip.Summary, lineSummary.Skip)
-	}
-
-	if !opts.Failed.Summary.Hide {
-		AddPrintLineSummary(&print, opts, opts.Failed.Summary, lineSummary.Failed)
-	}
-
-	if !opts.Ok.Summary.Hide {
-		AddPrintLineSummary(&print, opts, opts.Ok.Summary, lineSummary.Ok)
-	}
-
-	if !opts.ErrorThrown.Summary.Hide {
-		AddPrintLineSummary(&print, opts, opts.ErrorThrown.Summary, lineSummary.ErrorThrown)
-	}
-
-	if len(print) > 0 {
-		print = append(print, ColorANSI(opts, opts.Summary.Footer.Colors)...)
-		print = append(print, []byte(opts.Summary.Footer.Title)...)
-		print = append(print, ColorReset...)
-		print = append(print, []byte("\n")...)
-		return print
-	}
-
-	return nil
 }
